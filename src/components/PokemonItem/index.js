a76aa807d8b906f4ab70bc241d411e8ce7491229
Romer <romerramos@gmail.com>
2019-04-17 12:24:49 +0200

    Extraido componente PokemonTagList

--- src/components/PokemonItem/index.js
+++ src/components/PokemonItem/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import './PokemonItem.scss';
 import PokemonImage from '../PokemonImage';
+import PokemonTagList from '../PokemonTagList';
 
 const PokemonItem = () => (
   <div className="pokemon-item">
     <div className="pokemon-item__card">
       <PokemonImage/>
       <div className="pokemon-item__name">Bulbasaur</div>
-      <div className="pokemon-item__tags">
-        <div className="pokemon-item__tag">Poison</div>
-        <div className="pokemon-item__tag">Grass</div>
-      </div>
+      <PokemonTagList/>
       <div className="pokemon-item__parent">
         <div className="pokemon-item__bar"></div>
         <div className="pokemon-item__info">
---
+++ src/components/PokemonTagList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './PokemonTagList.scss';
+
+const PokemonTagList = () => (
+  <div className="pokemon-tag-list">
+    <div className="pokemon-tag-list__tag">Poison</div>
+    <div className="pokemon-tag-list__tag">Grass</div>
+  </div>
+);
+
+export default PokemonTagList;
